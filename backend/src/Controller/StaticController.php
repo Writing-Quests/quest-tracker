b77212bea9e69dc9dbd4dc40aa919972a1b3c0be
Byron Lutz <byronlutz@gmail.com>
2024-09-08 21:23:27 -0700

    Reconfigured vite/symfony integration without symlinks

--- backend/src/Controller/StaticController.php
+++ backend/src/Controller/StaticController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class StaticController extends AbstractController {
     public function index(): BinaryFileResponse {
-        $indexFile = $this->getParameter('kernel.project_dir') . '/../frontend/dist/index.html';
+        $indexFile = $this->getParameter('kernel.project_dir') . '/public/app/index.html';
         $response = new BinaryFileResponse($indexFile);
         $response->headers->set('Content-Type', 'text/html');
         return $response;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command}) => {
+  const opts = {
+    plugins: [react()],
+    build: {
+      outDir: '../backend/public/app',
+      emptyOutDir: true,
+    },
+  }
+  console.log(command)
+  if(command === 'build') {
+    opts.base = '/app'
+  }
+  return opts
 })
