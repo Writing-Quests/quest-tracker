75a8e270a01a7d9922b7fa2f79d1b0853d50041e
Byron Lutz <byronlutz@gmail.com>
2024-09-08 23:24:43 -0700

    Configured User entity (in database) and API Platform

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Quest Tracker enables individuals and Guides to track their progress on their wr
 
 1. Install [Lando](https://lando.dev/). The default Lando setup will install Docker Desktop & Docker Compose on your machine.
 1. Start Lando with `lando start`. This will take a while, especially on the first time.
+1. Run the database migrations with `lando php bin/console doctrine:migrations:migrate`
 1. Access the site in your browser at `frontend.quest-tracker.lndo.site` (for a hot-reloading version of react) or `backend.quest-tracker.lndo.site` (to access symfony directly and a built version of the react app).
 
 # FAQ
--- backend/src/Controller/StaticController.php
+++ backend/src/Controller/StaticController.php
@@ -3,13 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class StaticController extends AbstractController {
-    public function index(): BinaryFileResponse {
-        $indexFile = $this->getParameter('kernel.project_dir') . '/public/app/index.html';
-        $response = new BinaryFileResponse($indexFile);
-        $response->headers->set('Content-Type', 'text/html');
-        return $response;
+    public function index(): Response {
+        return $this->render('index.html');
     }
 }
